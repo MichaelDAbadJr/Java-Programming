1049cc67c6a5b6eec21b7fbb95377f55f2526b22
MichaelDAbadJr <58898444+MichaelDAbadJr@users.noreply.github.com>
2023-10-30 23:04:38 -0500

    Update Four18.java

--- Four18.java
+++ Four18.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+// Count Characters Write a program whose input is a character and a string, 
+// and whose output indicates the number of times the character appears in 
+// the string. The output should include the input character and use the plural form, 
+// n's, if the number of times the character appears is not exactly 1.
 public class Four18 {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
