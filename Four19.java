c1bfb9df817e578d0bac3a286083acf466e8ce55
MichaelDAbadJr <58898444+MichaelDAbadJr@users.noreply.github.com>
2023-10-30 23:09:32 -0500

    Update Four19.java

--- Four19.java
+++ Four19.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+// Forms often allow a user to enter an integer. Write a program that takes in a 
+// string representing an integer as input, and outputs Yes if every character is a digit 0-9.
 
 public class Four19 {
 	public static void main(String[] args) {
